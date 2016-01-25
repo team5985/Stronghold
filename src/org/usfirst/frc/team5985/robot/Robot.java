7f09718daef7ed6c11df620a8234f551310fe0fb
RK003 <zac-hah@hotmail.co.uk>
2016-01-25 18:26:35 +1100

    Added & removed some comments, also a little line in .gitignore

--- src/org/usfirst/frc/team5985/robot/Robot.java
+++ src/org/usfirst/frc/team5985/robot/Robot.java
@@ -24,8 +24,6 @@ import edu.wpi.first.wpilibj.CameraServer;
 public class Robot extends IterativeRobot {
 	RobotDrive myRobot;
 	Joystick stick;
-//	Servo servoBot;
-//	Servo servoTop;
 	Encoder intakeEncoder;
 	CameraServer camera1;
 	//CameraServer camera2;
@@ -36,7 +34,6 @@ public class Robot extends IterativeRobot {
 	DigitalInput intakeLimitSwitchUp;
 	DigitalInput intakeLimitSwitchDown;
 	int autoLoopCounter;
-	boolean intakeIsUp;    //help i don't know what i'm doing i'll put this here anyways ~Zac
 	
     /**
      * This function is run when the robot is first started up and should be
@@ -51,8 +48,6 @@ public class Robot extends IterativeRobot {
     	intakeLimitSwitchDown = new DigitalInput(4);
     	intakeEncoder = new Encoder(1,2,true);    //Ports 1,2 used by intake.
     	intakeMotor = new VictorSP(2);
-//    	servoBot = new Servo(2);
-//    	servoTop = new Servo(3);
 //    	lineSensor = new DigitalInput(0);
     	camera1 = CameraServer.getInstance();
     	camera1.setQuality(50);
@@ -116,7 +111,7 @@ public class Robot extends IterativeRobot {
     	
     	intake();
     	
-    	//arm();
+    	//arm();    TODO build and test the arm
     	
     	//System.out.println("teleopPeriodic: Stick x = " + stick.getX() + " y = " + stick.getY());
     }
@@ -133,21 +128,24 @@ public class Robot extends IterativeRobot {
     {
     	// Driving code for the robot
     	
+    	//Sets the speedModifier to the throttle
     	double speedModifier = (stick.getThrottle() - 1 ) / 2;
-    	
     	
     	System.out.println("speedModifier:'" + speedModifier +"'");
     	
+    	//Gets stick values and sets variables for it
     	double steering = stick.getX() * speedModifier;
     	double power = stick.getY() * speedModifier;
     	
+    	//Applies above variables to variables that will be applied to the motors
     	double leftPower = -power + -steering;
     	double rightPower = power + -steering;
     	
+    	//Displays the above variables (the motor powers)
     	System.out.println("leftPower:'" + leftPower +"'");
     	System.out.println("rightPower:'" + rightPower +"'");
     			
-    	// TODO: test this on the actual robot, the signs are probably wrong 
+    	//Sends the power variables to the motors
     	driveLeft.set(leftPower);
     	driveRight.set(rightPower);
     }
