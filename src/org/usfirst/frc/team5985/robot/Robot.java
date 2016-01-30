cc6b95dfcc829edc6c8cb52ba0394fc5670aceea
Joshua Willigers <jwilligers@gmail.com>
2016-01-30 14:35:03 +1100

    yay

--- src/org/usfirst/frc/team5985/robot/Robot.java
+++ src/org/usfirst/frc/team5985/robot/Robot.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team5985.robot;
 
+// This touchpad is awful
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
