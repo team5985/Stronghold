588e60e35c0ed1ec8438dfcb45373110910f8ff5
Joe Programmer <coach@projectb.net.au>
2016-06-04 16:04:38 +1000

    Added Gesture Sensor readings; failing for unknown reason, possible solution different port. ~Isaac & Zac

--- src/org/usfirst/frc/team5985/robot/Intake.java
+++ src/org/usfirst/frc/team5985/robot/Intake.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team5985.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 /*
@@ -13,10 +15,10 @@ public class Intake {
 	
 	
 	private VictorSP _motor;
-	private DigitalInput _intakeLimitSwitch;
+	//private DigitalInput _intakeLimitSwitch;
+	private I2C _I2C;
 	
-	
-	
+	private boolean ball;
 	/*
 	 * Constructor: pass in joystick, intake motor, and intake limit switch
 	 */
@@ -25,8 +27,10 @@ public class Intake {
 		System.out.println("Intake Constructor Called!");	
 		_motor = new VictorSP(motorIn_PWM_Port);
 		
-		_intakeLimitSwitch = new DigitalInput(switchIn_DIO_Port);
+		//_intakeLimitSwitch = new DigitalInput(switchIn_DIO_Port);
     	
+		_I2C = new I2C(I2C.Port.kOnboard,0);
+		
 	}
 	
     /**
@@ -38,7 +42,35 @@ public class Intake {
      */
 	public void handleEvents(DriverStation driverStation)
 	{ 	
-		if (driverStation.stick.getRawButton(1) && !_intakeLimitSwitch.get())
+		byte[] leftRange = new byte[1];
+		byte[] rightRange = new byte[1];
+		SmartDashboard.putNumber("Left Range = ",leftRange[0]);
+		SmartDashboard.putNumber("Right Range = ",rightRange[0]);
+		
+		boolean result1 = _I2C.read(0x0c,1,leftRange);
+		boolean result2 =_I2C.read(0x0e,1,rightRange);
+		
+		if (result1 || result2 == false){
+			System.out.println("Found a signal");
+		}
+		else{
+			System.out.println("No Signal Found.");
+		}
+		SmartDashboard.putBoolean("Left Read = ",!result1);
+		SmartDashboard.putBoolean("Right Read = ",!result2);
+		
+		int range = (leftRange[0] * rightRange[0])/20; // range ~ distance in cm
+		SmartDashboard.putNumber("Range = ",range);
+		if (range < 20)
+		{
+			ball = true;
+		}
+		else
+		{
+			ball = false;
+		}
+		
+		if (driverStation.stick.getRawButton(1) && !ball)//_intakeLimitSwitch.get())
 		{
 			//button 1 and boulder not completely in: suck in boulder	
 			_motor.set(-0.6);
@@ -53,7 +85,7 @@ public class Intake {
 			//no buttons or button 1 and boulder completely in: stop moving
 			_motor.stopMotor();
 		}
-		System.out.println("Limit Switch: '" + _intakeLimitSwitch.get() + "'");
+		System.out.println(/*"Limit Switch: '"*/"Gesture Sensor Proximity: '" + ball + "'");//_intakeLimitSwitch.get() + "'");
 	}
 	
 	/**
@@ -62,6 +94,6 @@ public class Intake {
 	 */
 	public boolean hasBoulder()
 	{
-		return _intakeLimitSwitch.get();
+		return ball;//_intakeLimitSwitch.get();
 	}
 }
